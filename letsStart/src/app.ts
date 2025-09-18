ed3892fd5e9b2cc33afc2ed944204bfbcb4eb8f1
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-09-18 13:18:38 +0900

    init: add app src with proper .gitignore

---
+++ letsStart/src/app.ts
@@ -0,0 +1,12 @@
+import * as express from "express";
+const app: express.Express = express();
+const port: number = 9000;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  console.log(req);
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
