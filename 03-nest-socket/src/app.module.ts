0951eced6a0d07ff73b5ccec04c06c6695dc3ad2
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-12-06 15:02:04 +0900

    MVC 패턴 분리 및 프로젝트 셋업

--- 03-nest-socket/src/app.module.ts
+++ 03-nest-socket/src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure() {
+    const DEBUG = process.env.MODE === 'dev' ? true : false;
+    mongoose.set('debug', DEBUG);
+  }
+}
